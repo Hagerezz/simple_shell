15109d7f5c068b67f00065dc3af55dbc68f499a0
Hagerezz <queen.emilly12@gmail.com>
2023-05-25 01:10:48 +0300

    i hope it works

--- shell.c
+++ shell.c
@@ -1,8 +1,5 @@
 #include "shell.h"
 
-#define MAX_ARGS 64
-#define DELIMITER " \t\r\n\a"
-
 /**
  * split_line - Split a line into an array of tokens
  * @line: The input line to be split
@@ -11,38 +8,25 @@
  */
 char **split_line(char *line)
 {
-    int bufsize = MAX_ARGS;
-    int position = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    char *token;
+	int bufsize = MAX_ARGS;
+	int position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
 
-    if (!tokens)
-    {
-        fprintf(stderr, "Allocation error\n");
-        exit(EXIT_FAILURE);
-    }
+	if (!tokens)
+	{
+		fprintf(stderr, "Allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
-    token = strtok(line, DELIMITER);
-    while (token != NULL)
-    {
-        tokens[position] = token;
-        position++;
-
-        if (position >= bufsize)
-        {
-            bufsize += MAX_ARGS;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens)
-            {
-                fprintf(stderr, "Allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        token = strtok(NULL, DELIMITER);
-    }
-    tokens[position] = NULL;
-    return (tokens);
+	token = strtok(line, DELIMITER);
+	while (token != NULL)
+	{
+		tokens[position++] = token;
+		token = strtok(NULL, DELIMITER);
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
 
 /**
@@ -51,51 +35,39 @@ char **split_line(char *line)
  */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    char **args;
-    int status, r;
-    char *s = "./shell: No such file or directory\n";
-    pid_t pid;
+	char *line = NULL;
+	size_t len = 0;
+	char **args;
+	int status, r;
 
-    while (1)
-    {
-        write(STDOUT_FILENO, "#cisfun$ ", 9);
-        r = getline(&line, &len, stdin);
-        if (r == -1)
+	while (1)
+	{
+		printf("#cisfun$ ");
+		r = getline(&line, &len, stdin);
+		if (r == -1)
         {
             exit(EXIT_FAILURE);
         }
-
+        
         args = split_line(line);
 
-        if (args[0] == NULL)
-            continue;
+		if (args[0] == NULL)
+			continue;
 
-        if (access(args[0], F_OK) == -1)
-        {
-            write(STDOUT_FILENO, s, 35);
-            continue; /* Do not fork if command does not exist */
-        }
-        else
-        {
-            pid = fork();
-            if (pid == 0)
-            {
-                execve(args[0], args, NULL);
-                perror("./shell");
-                exit(0);
-            }
-            else if (pid < 0)
-            {
-                perror("fork");
-                exit(0);
-            }
-            else
-                waitpid(pid, &status, 0);
-        }
-        free(line);
-        free(args);
-    }
-    return (0);
+		if (access(args[0], F_OK) == -1)
+		{
+			printf("%s: Command not found.\n", args[0]);
+			continue;  /* Do not fork if command does not exist */
+		}
+		else
+		{
+			status = execve(args[0], args, NULL);
+			if (status == -1)
+				perror("execve");
+		}
+	}
+
+	free(line);
+	free(args);
+	return (0);
 }
